f88e574ee4993fc99317b8f3ac87371f9148bdad
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-01-20 08:00:43 +0200

    init fonts and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./src/**/*.svelte"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#EDF7F6',
+        'secondary': '#161626',
+        'tertiary': '#FCDC4D',
+        'green': '#00A676',
+        'blue': '#226CE0',
+        'purple': '#C455A8',
+        'orange': '#FC5130',
+      },
+      fontFamily: {
+        'slab': 'Montagu Slab, serif',
+        'sans': 'Sora, sans-serif',
+      },
+    },
   },
   plugins: [],
 }
