b1ab9351f6cf69f624f4d1376b749466f4acbe1f
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-01-20 08:54:20 +0200

    base text styles

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,4 @@
 <slot></slot>
-<style>
-    @import url('https://fonts.googleapis.com/css2?family=Montagu+Slab:wght@100;200;400;700&family=Sora:wght@100;200;400;700&display=swap');
-    @tailwind base;
-    @tailwind components;
-    @tailwind utilities;
-</style>
+<script>
+    import '../styles/tailwind.css'
+</script>
