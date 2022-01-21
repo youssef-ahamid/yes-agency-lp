48ad94fc7221bae97aba683a99fb979858b27b3b
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-01-21 18:23:45 +0200

    project tab component

---
+++ src/components/ui/ProjectTab.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let slug;
+</script>
+
+<div class="flex justify-between items-start flex-col-reverse md:flex-row w-full max-w-xl">
+	<div>
+		<a href={`/projects/${slug}`}>
+			<h4 class="hover:text-tertiary transition duration-300 ease-out"><slot name="title" /></h4>
+		</a>
+		<p><slot name="subtitle" /></p>
+	</div>
+	<div class="w-48 md:w-64">
+		<slot name="tags" />
+	</div>
+</div>
