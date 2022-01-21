13b92960c71e040f06e296f1285825adbc4d2a5a
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-01-21 17:05:37 +0200

    text prop -> slot in button and tag + refactor

--- src/components/base/Button.svelte
+++ src/components/base/Button.svelte
@@ -1,6 +1,5 @@
 <script>
-	export let text,
-        to,
+	export let to,
 		round,
 		ghost,
 		underlined,
@@ -15,50 +14,58 @@
 </script>
 
 <a href={to}>
-    <button
-        class="py-3 md:py-4 px-8 md:px-10 accent-primary rounded-full flex flex-wrap items-center group transition duration-300 ease-out focus:ring-4 focus:ring-offset-4 focus:ring-current"
-        class:hover:shadow-md={round}
-        class:hover:bg-primary={round}
-        class:bg-primary={round && primary}
-        class:bg-secondary={round && secondary}
-        class:bg-tertiary={round && tertiary}
-        class:bg-blue={round && blue}
-        class:bg-orange={round && orange}
-        class:bg-green={round && green}
-        class:bg-purple={round && purple}
-        class:text-primary={primary}
-        class:text-secondary={secondary}
-        class:text-tertiary={tertiary}
-        class:text-blue={blue}
-        class:text-orange={orange}
-        class:text-green={green}
-        class:text-purple={purple}
-    >
-        <a
-            href={to}
-            class:text-secondary={(tertiary && round) || (primary && round) || (secondary && ghost)}
-            class:text-tertiary={tertiary && ghost}
-            class:text-blue={blue && ghost}
-            class:text-orange={orange && ghost}
-            class:text-green={green && ghost}
-            class:text-purple={purple && ghost}
-            class:group-hover:text-secondary={round}
-            class="transition duration-300 ease-out"  
-        >{text}</a>
-        {#if icon}
-            <img class="w-4 h-4 ml-3 md:w-6 md:h-6 md:ml-5" src={icon} alt="icon" />
-        {/if}
-        {#if underlined}
-            <div
-                class="h-1 rounded-full my-1 w-full transition duration-300 ease-out"
-                class:bg-primary={primary}
-                class:bg-secondary={secondary}
-                class:bg-tertiary={tertiary}
-                class:bg-blue={blue}
-                class:bg-orange={orange}
-                class:bg-green={green}
-                class:bg-purple={purple}
-            />
-        {/if}
-    </button>
+	<button
+		class="py-2 md:py-4 px-6 md:px-10 hover:shadow-primary/20 rounded-full flex flex-wrap items-center group transition duration-300 ease-out focus:ring-4 focus:ring-offset-4 focus:ring-current"
+		class:hover:shadow-lg={round}
+		class:hover:bg-primary={round}
+		class:bg-primary={round && primary}
+		class:bg-secondary={round && secondary}
+		class:bg-tertiary={round && tertiary}
+		class:bg-blue={round && blue}
+		class:bg-orange={round && orange}
+		class:bg-green={round && green}
+		class:bg-purple={round && purple}
+		class:text-primary={primary}
+		class:text-secondary={secondary}
+		class:text-tertiary={tertiary}
+		class:text-blue={blue}
+		class:text-orange={orange}
+		class:text-green={green}
+		class:text-purple={purple}
+	>
+		<a
+			href={to}
+			class:text-secondary={(tertiary && round) || (primary && round) || (secondary && ghost)}
+			class:text-tertiary={tertiary && ghost}
+			class:text-blue={blue && ghost}
+			class:text-orange={orange && ghost}
+			class:text-green={green && ghost}
+			class:text-purple={purple && ghost}
+			class:group-hover:text-secondary={round}
+			class="transition duration-300 ease-out"><slot /></a
+		>
+		{#if icon}
+			<img class="w-4 h-4 ml-3 md:w-6 md:h-6 md:ml-5" src={icon} alt="icon" />
+		{/if}
+		{#if underlined || ghost}
+			<div class="relative w-full h-3">
+				{#if underlined}
+					<div
+						class="h-1 rounded-full my-1 w-full transition duration-300 ease-out"
+						class:bg-primary={primary}
+						class:bg-secondary={secondary}
+						class:bg-tertiary={tertiary}
+						class:bg-blue={blue}
+						class:bg-orange={orange}
+						class:bg-green={green}
+						class:bg-purple={purple}
+					/>
+				{/if}
+
+				<div
+					class="h-1 rounded-full absolute top-1 transition duration-300 ease-out bg-primary w-full scale-x-0 group-hover:scale-x-100"
+				/>
+			</div>
+		{/if}
+	</button>
 </a>
