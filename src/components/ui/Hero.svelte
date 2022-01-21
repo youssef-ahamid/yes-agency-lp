2115a4cdc4434c5551e1d70e07b3f767364bbf82
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-01-21 17:04:27 +0200

    Hero

---
+++ src/components/ui/Hero.svelte
@@ -0,0 +1,12 @@
+<script>
+	import ScrollDown from '../base/ScrollDown.svelte';
+</script>
+
+<div class="min-h-screen w-full flex justify-center items-center -mt-24">
+	<div class="max-w-7xl w-full flex flex-wrap justify-start items-center px-8">
+		<slot />
+		<div class="absolute right-2 bottom-2">
+			<ScrollDown />
+		</div>
+	</div>
+</div>
