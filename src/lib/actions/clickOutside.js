75b942f800a83eb7f8eb0c74fbf9df6a2ad3e3cf
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-01-27 10:36:16 +0200

    click outside action (for mobile nav)

---
+++ src/lib/actions/clickOutside.js
@@ -0,0 +1,17 @@
+const clickOutside = function (node) {
+	const handleClick = (event) => {
+		if (!node.contains(event.target)) {
+			node.dispatchEvent(new CustomEvent('outclick'));
+		}
+	};
+
+	document.addEventListener('click', handleClick, true);
+
+	return {
+		destroy() {
+			document.removeEventListener('click', handleClick, true);
+		}
+	};
+};
+
+export default clickOutside;
