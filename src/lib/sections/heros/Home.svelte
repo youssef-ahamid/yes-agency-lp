c281c3954b3663f0b9abcce6ca1c8919c889b6c4
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-01-27 12:38:04 +0200

    reference  created hero secctions to enable smooth scroll to it

--- src/lib/sections/heros/Home.svelte
+++ src/lib/sections/heros/Home.svelte
@@ -1,10 +1,13 @@
 <script>
+	import { scrollRef } from 'svelte-scrolling';
+	export const id = 'home';
+
 	import { onMount } from 'svelte';
 
 	import Navbar from '$lib/components/ui/Navbar.svelte';
 	import Button from '$lib/components/base/Button.svelte';
 	import Hero from '$lib/components/ui/Hero.svelte';
-	
+
 	export let title, subtitle, words;
 	export let speed = 1;
 	export let actions = [];
@@ -39,35 +42,36 @@
 	onMount(() => setup());
 </script>
 
-<Navbar color={words[index].color} />
-<Hero long>
-	<div class="max-w-3xl py-2 px-2">
-		<h1 class="my-4 md:my-6">
-			{title}
-			{#if textVisible}
-				<span
-					class={`underline underline-offset-4 decoration-${words[index].color}`}
-					transition:typewriter
-					on:introend={backspace}
-					on:outroend={next}>{words[index].text}</span
-				>
-			{/if}
-		</h1>
-		<p>{subtitle}</p>
-		<div class="my-8 flex w-full">
-			{#each actions as action}
-				<Button
-					{...action}
-					blue={words[index].color == 'blue'}
-					purple={words[index].color == 'purple'}
-					orange={words[index].color == 'orange'}
-					green={words[index].color == 'green'}
-					tertiary={words[index].color == 'tertiary'}
-					secondary={words[index].color == 'secondary'}
-					primary={words[index].color == 'primary'}
+<section use:scrollRef={id}>
+	<Navbar color={words[index].color} />
+	<Hero long>
+		<div class="max-w-3xl py-2 px-2">
+			<h1 class="my-4 md:my-6">
+				{title}
+				{#if textVisible}
+					<span
+						class={`underline underline-offset-4 decoration-${words[index].color}`}
+						transition:typewriter
+						on:introend={backspace}
+						on:outroend={next}>{words[index].text}</span
+					>
+				{/if}
+			</h1>
+			<p>{subtitle}</p>
+			<div class="my-8 flex w-full">
+				{#each actions as action}
+					<Button
+						{...action}
+						blue={words[index].color == 'blue'}
+						purple={words[index].color == 'purple'}
+						orange={words[index].color == 'orange'}
+						green={words[index].color == 'green'}
+						tertiary={words[index].color == 'tertiary'}
+						secondary={words[index].color == 'secondary'}
+						primary={words[index].color == 'primary'}
 					/>
-				
-			{/each}
+				{/each}
+			</div>
 		</div>
-	</div>
-</Hero>
+	</Hero>
+</section>
