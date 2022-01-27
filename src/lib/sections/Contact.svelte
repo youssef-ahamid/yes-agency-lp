4243195db6339f1cb9d725b028a8f8585633656a
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-01-27 13:48:56 +0200

    contact us section

---
+++ src/lib/sections/Contact.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { scrollRef } from 'svelte-scrolling';
+	export const id = 'connect';
+	
+	import TextInput from '$lib/components/base/textInput.svelte';
+		
+	export let title = '';
+	export let text = '';
+	export let action = { text: 'start', primary: true, round: true, to: '/' };
+	
+	let value = '';
+</script>
+
+<section class="mx-auto max-w-7xl px-4 py-8" use:scrollRef={id}>
+	<h1 class="my-7 md:my-12">{title}</h1>
+	<p class="max-w-2xl">{text}</p>
+	<TextInput bind:value placeholder="Work email" big trim {action} />
+</section>
