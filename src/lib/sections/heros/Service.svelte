96a524c9f2c1028daca31851d5d4ecab6845933a
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-01-27 15:33:52 +0200

    service hero section

---
+++ src/lib/sections/heros/Service.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { scrollRef } from 'svelte-scrolling';
+	export const id = 'service-abstract';
+
+	import Button from '$lib/components/base/Button.svelte';
+	import Hero from '$lib/components/ui/Hero.svelte';
+	import ImageCard from '$lib/components/ui/ImageCard.svelte';
+
+	export let service;
+</script>
+
+<section use:scrollRef={id}>
+	<Hero long>
+		<div class="max-w-3xl p-4">
+			<div class="-ml-6 md:-ml-10">
+				<Button {...service.action} linkWithin={false} underlined text="" to="/services"
+					>/services/</Button
+				>
+			</div>
+			<h1 class={`my-4 capitalize md:my-6 text-${service.color}`}>{service.name}</h1>
+			<p>{service.description}</p>
+		</div>
+		<div>
+			<ImageCard src={service.main_image} action={service.action} preset={service.color} />
+		</div>
+	</Hero>
+</section>
