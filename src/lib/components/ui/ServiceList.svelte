c104e42b4c603bf9e159af592f54d5e40414d6ef
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-01-27 10:41:18 +0200

    service list component

---
+++ src/lib/components/ui/ServiceList.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let services;
+	import Button from '$lib/components/base/Button.svelte';
+	import Service from '$lib/components/ui/Service.svelte';
+</script>
+
+{#each services as service, i}
+	<Service flip={(i + 1) % 2 == 0}>
+		<span slot="title">{service.title}</span>
+		<span slot="text">{service.text}</span>
+		<span slot="number">{i + 1}</span>
+		<img src={service.img} slot="image" alt={service.title} class="block w-full" />
+		<Button {...service.action} slot="button" />
+	</Service>
+{/each}}
