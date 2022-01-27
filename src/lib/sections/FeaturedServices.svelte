2fdc9e54f4b0e28efaf4f35802e4d4a726db2b16
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-01-27 15:43:05 +0200

    featured services section

---
+++ src/lib/sections/FeaturedServices.svelte
@@ -0,0 +1,7 @@
+<script>
+	import ServiceList from '$lib/components/ui/ServiceList.svelte';
+	export let services;
+</script>
+
+<h1 class="my-14 text-center">What We Do</h1>
+<ServiceList {services} />
