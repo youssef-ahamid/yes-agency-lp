c38be5a8f6930757cd2c7ac1ac5cc98be4cfccc6
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-01-27 10:30:15 +0200

    goes to component

---
+++ src/lib/components/base/GoesTo.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let to = '';
+	export let text = false;
+</script>
+
+<div class="relative inline-block">
+	<a href={to} class="peer hover:text-primary">
+		{#if !text}
+			<slot />
+		{:else}
+			{text}
+		{/if}
+	</a>
+	<div
+		class="absolute top-[100%] h-px w-full origin-left scale-x-0 transform rounded-full bg-primary transition duration-200 ease-out peer-hover:scale-x-100"
+	/>
+</div>
