b4fdaf7eda67ab6e3ccde73c577eb1a1447db8a3
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-01-28 02:58:03 +0200

    allow explicit inversion of tag and clean up file

--- src/lib/components/base/Tag.svelte
+++ src/lib/components/base/Tag.svelte
@@ -1,33 +1,14 @@
 <script>
 	export let color, text;
+	export let inverted = false;
+
+	$: invertColor = color == 'primary' ? 'secondary' : 'primary';
 </script>
 
 <span
-	class="body-sm my-1 mr-1 inline-block max-w-fit rounded-full border-2 border-transparent py-1 px-3 font-bold uppercase transition duration-300 ease-out hover:bg-opacity-0 hover:shadow hover:shadow-current md:mr-3 md:px-5"
-	class:bg-primary={color == 'primary'}
-	class:bg-secondary={color == 'secondary'}
-	class:bg-tertiary={color == 'tertiary'}
-	class:bg-blue={color == 'blue'}
-	class:bg-orange={color == 'orange'}
-	class:bg-green={color == 'green'}
-	class:bg-purple={color == 'purple'}
-	class:bg-gray-400={color == 'disabled'}
-	class:hover:border-primary={color == 'primary'}
-	class:hover:border-secondary={color == 'secondary'}
-	class:hover:border-tertiary={color == 'tertiary'}
-	class:hover:border-blue={color == 'blue'}
-	class:hover:border-orange={color == 'orange'}
-	class:hover:border-green={color == 'green'}
-	class:hover:border-purple={color == 'purple'}
-	class:hover:border-gray-400={color == 'disabled'}
-	class:hover:text-primary={color == 'primary'}
-	class:hover:text-secondary={color == 'secondary'}
-	class:hover:text-tertiary={color == 'tertiary'}
-	class:hover:text-blue={color == 'blue'}
-	class:hover:text-orange={color == 'orange'}
-	class:hover:text-green={color == 'green'}
-	class:hover:text-purple={color == 'purple'}
-	class:hover:text-gray-400={color == 'disabled'}
+	class="body-sm hover:shadow-color/20 my-1 mr-1 inline-block max-w-fit rounded-full border-2 py-1 px-3 font-bold uppercase transition duration-300 ease-out hover:shadow md:mr-3 md:px-5 {!inverted
+		? `border-transparent hover:bg-opacity-0 bg-${color} hover:border-${color} hover:text-${color}`
+		: `text-${invertColor} border-${invertColor} hover:text-${color} hover:bg-${invertColor} hover:border-${color}`}"
 >
 	{#if text}
 		{text}
