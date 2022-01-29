ca894dce7b0892fd04b2a918eedce51ede1a998e
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-01-29 22:28:58 +0200

    budget like a pro

---
+++ src/lib/components/ui/Onboarding/Budget.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Checkbox from '$lib/components/base/Checkbox.svelte';
+	import RangeSlider from '$lib/components/base/RangeSlider.svelte';
+
+	export let title, subtitle, min, max;
+	export let step = Math.floor((max - min) / 100);
+	export let def = min;
+	export let budget = def;
+	export let notSure = false;
+</script>
+
+<h4>{title}</h4>
+<h6 class="max-w-3xl py-4 pr-2 font-thin">{subtitle}</h6>
+
+<div class="w-52 md:w-full">
+	<RangeSlider {min} {max} {step} bind:value={budget} beyond money />
+</div>
+<Checkbox label="I'm not 100% sure" bind:checked={notSure} />
