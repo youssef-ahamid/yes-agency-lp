14d9fe25a67f796843cf3b0976016c3a4bacd74d
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-01-29 08:18:16 +0200

    service projects section

---
+++ src/lib/sections/ServiceProjects.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { scrollRef } from 'svelte-scrolling';
+	export const id = 'service-projects';
+
+	import WorkCard from '$lib/components/ui/WorkCard.svelte';
+
+	export let service = {
+		name: 'branding',
+		color: 'purple'
+	};
+	export let projects;
+</script>
+
+<section class="mx-auto flex max-w-7xl flex-wrap justify-center" use:scrollRef={id}>
+	<h2 class="my-6 w-full text-center">
+		Past <span
+			class="underline-offset-6 underline decoration-{service.color} decoration-6 capitalize"
+			>{service.name}</span
+		> Work
+	</h2>
+
+	{#each projects as project}
+		<WorkCard slug={project.slug} color={service.color}>
+			<img src={project.client.logo} slot="logo" alt={project.client.name} />
+			<span slot="title">{project.title}</span>
+			<span slot="dates">{project.dates}</span>
+		</WorkCard>
+	{/each}
+</section>
