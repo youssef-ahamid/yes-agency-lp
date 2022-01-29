cdc86b4b6ba054c095775fe74e7d272dabb5fdd3
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-01-29 21:11:37 +0200

    personal info step

---
+++ src/lib/components/ui/Onboarding/PersonalInfo.svelte
@@ -0,0 +1,15 @@
+<script>
+	import TextInput from '$lib/components/base/textInput.svelte';
+	export let questions, title, subtitle;
+</script>
+
+<h4>{title}</h4>
+<h6 class="max-w-3xl pr-2 pt-4 font-thin">{subtitle}</h6>
+
+<div class="flex flex-wrap">
+	{#each questions as question}
+		<div class="mt-10 w-full md:w-1/2">
+			<TextInput {...question} bind:value={question.value} />
+		</div>
+	{/each}
+</div>
