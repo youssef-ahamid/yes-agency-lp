a00c75c2fffe79296e68d274a1448cbcc4160335
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-01-29 21:54:42 +0200

    service selection step

---
+++ src/lib/components/ui/Onboarding/ServiceSelect.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Checkbox from '$lib/components/base/Checkbox.svelte';
+	export let options, title, subtitle;
+    export let selected = [];
+</script>
+
+<h4>{title}</h4>
+<h6 class="max-w-3xl pr-2 pt-4 font-thin">{subtitle}</h6>
+
+<div class="flex flex-wrap">
+	{#each options as option}
+		<div class="mt-10 w-full md:w-1/3">
+			<Checkbox {...option} bind:group={selected} />
+		</div>
+	{/each}
+</div>
