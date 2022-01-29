dba03fa254a70f0fded9fb635df78b0eb673b51a
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-01-29 13:41:28 +0200

    lint

--- src/lib/components/base/Checkbox.svelte
+++ src/lib/components/base/Checkbox.svelte
@@ -1,10 +1,20 @@
 <script>
-    export let checked, label;
-    export let color = "primary";
+	export let checked, label;
+	export let color = 'primary';
 </script>
 
-<label class="flex items-center pb-2 max-w-min relative group cursor-pointer">
-	<input type=checkbox bind:checked class="peer w-5 h-5 md:w-6 md:h-6 rounded-md appearance-none outline-none border-2 border-primary border-opacity-50 group-hover:border-opacity-80 focus:border-opacity-80 transition duration-200 ease-out bg-transparent checked:bg-{color} checked:border-opacity-100 focus:ring-1 focus:ring-{color} checked:ring-{color} cursor-pointer">
-	<h6 class="text-primary text-opacity-50 peer-checked:text-{color} group-hover:text-opacity-80 peer-focus:text-opacity-80 peer-checked:text-opacity-100 transition duration-300 ease-out ml-4">{label}</h6>
-    <div class="absolute top-full w-full scale-x-0 group-hover:scale-x-[20%] peer-checked:scale-x-100 origin-left bg-{color} h-px transition duration-300 ease-out rounded-full"></div>
+<label class="group relative flex max-w-min cursor-pointer items-center pb-2">
+	<input
+		type="checkbox"
+		bind:checked
+		class="peer h-5 w-5 appearance-none rounded-md border-2 border-primary border-opacity-50 bg-transparent outline-none transition duration-200 ease-out focus:border-opacity-80 group-hover:border-opacity-80 md:h-6 md:w-6 checked:bg-{color} checked:border-opacity-100 focus:ring-1 focus:ring-{color} checked:ring-{color} cursor-pointer"
+	/>
+	<h6
+		class="text-primary text-opacity-50 peer-checked:text-{color} ml-4 mr-1 transition duration-300 ease-out group-hover:text-opacity-80 peer-checked:text-opacity-100 peer-focus:text-opacity-80"
+	>
+		{label}
+	</h6>
+	<div
+		class="absolute top-full w-full origin-left scale-x-0 group-hover:scale-x-[20%] peer-checked:scale-x-100 bg-{color} h-px rounded-full transition duration-300 ease-out"
+	/>
 </label>
