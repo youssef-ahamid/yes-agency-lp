46179cc6f0f857d1d641128cf43d11f7d30f248c
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-01-30 09:59:25 +0200

    bnd to checked state of option passed to service select

--- src/lib/components/ui/Onboarding/ServiceSelect.svelte
+++ src/lib/components/ui/Onboarding/ServiceSelect.svelte
@@ -1,7 +1,6 @@
 <script>
 	import Checkbox from '$lib/components/base/Checkbox.svelte';
 	export let options, title, subtitle;
-    export let selected = [];
 </script>
 
 <h4>{title}</h4>
@@ -10,7 +9,7 @@
 <div class="flex flex-wrap">
 	{#each options as option}
 		<div class="mt-10 w-full md:w-1/3">
-			<Checkbox {...option} bind:group={selected} />
+			<Checkbox {...option} bind:checked={option.checked} />
 		</div>
 	{/each}
 </div>
