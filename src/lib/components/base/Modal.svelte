93091130a0c9e862cafaa573cbb4358038baea42
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-02-01 09:46:44 +0200

    click outside to close modal

--- src/lib/components/base/Modal.svelte
+++ src/lib/components/base/Modal.svelte
@@ -1,17 +1,20 @@
 <script>
+	import clickOutside from '$lib/actions/clickOutside';
 	import { fade, fly } from 'svelte/transition';
 	export let active = false;
 </script>
 
 {#if active}
 	<div
-		class="fixed z-50 flex h-screen w-screen items-center justify-center bg-secondary bg-opacity-75"
+		class="fixed inset-0 z-[999] flex h-screen w-screen items-center justify-center bg-secondary bg-opacity-75"
 		transition:fade={{ duration: 300 }}
 	>
 		<div
 			class="max-h-fit w-full max-w-6xl flex-1 overflow-hidden rounded-3xl bg-secondary-light p-12 text-primary md:rounded-[50px] md:p-20"
 			id="modal"
 			transition:fly={{ duration: 300, y: 50, delay: 200 }}
+			use:clickOutside
+			on:outclick={() => (active = false)}
 		>
 			<slot />
 		</div>
