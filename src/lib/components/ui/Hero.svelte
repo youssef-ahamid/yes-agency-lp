56c5c006ae67a693d58a598dbad392d22121ce57
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-02-01 14:48:47 +0200

    improve hero layout

--- src/lib/components/ui/Hero.svelte
+++ src/lib/components/ui/Hero.svelte
@@ -3,7 +3,10 @@
 	export let long;
 </script>
 
-<div class="flex min-h-screen w-full items-center justify-center lg:-mt-28" class:mt-8={long}>
+<div
+	class="flex min-h-screen w-full items-center justify-center pt-12 lg:-mt-28"
+	class:pt-28={long}
+>
 	<div
 		class="flex w-full max-w-7xl flex-wrap items-center justify-start overflow-scroll px-2 md:justify-between"
 	>
