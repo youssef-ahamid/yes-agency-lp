9430119d94b87f19ff2cbe37b915c269ab57e9d1
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-02-01 09:46:14 +0200

    open onboarding stepper when contact form submitted

--- src/lib/sections/Contact.svelte
+++ src/lib/sections/Contact.svelte
@@ -2,17 +2,28 @@
 	import { scrollRef } from 'svelte-scrolling';
 	export const id = 'connect';
 
+	import Stepper from '$lib/components/ui/Stepper.svelte';
 	import TextInput from '$lib/components/base/textInput.svelte';
 
 	export let title = '';
 	export let text = '';
-	export let action = { text: 'start', primary: true, round: true, to: '/' };
+	export let action = { text: 'start', primary: true, round: true };
+	export let onboarding = {};
 
 	let value = '';
+	let open = false;
 </script>
 
 <section class="mx-auto max-w-7xl px-4 py-8" use:scrollRef={id}>
 	<h1 class="my-7 md:my-12">{title}</h1>
 	<p class="max-w-2xl">{text}</p>
-	<TextInput bind:value placeholder="Work email" big trim {action} />
+	<TextInput
+		bind:value
+		placeholder="Work email"
+		big
+		trim
+		{action}
+		on:submit={() => (open = true)}
+	/>
 </section>
+<Stepper bind:open steps={onboarding.steps} success={onboarding.success} />
