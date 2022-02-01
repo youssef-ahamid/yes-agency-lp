fbadbe3bc1d6fdff4ecedecdee983d7f3abc1908
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-02-01 09:44:37 +0200

    project tab improvements

--- src/lib/components/ui/ProjectTab.svelte
+++ src/lib/components/ui/ProjectTab.svelte
@@ -1,15 +1,18 @@
+<svelte:options immutable={true} />
+
 <script>
+	import Goes from '$lib/components/base/GoesTo.svelte';
 	export let slug;
 </script>
 
-<div class="flex w-full max-w-xl flex-col-reverse items-start justify-between md:flex-row">
+<div class="flex w-full max-w-2xl items-start justify-between">
 	<div>
-		<a href={`/projects/${slug}`}>
+		<Goes to="/projects/{slug}">
 			<h4 class="transition duration-300 ease-out hover:text-tertiary"><slot name="title" /></h4>
-		</a>
-		<p><slot name="subtitle" /></p>
+		</Goes>
+		<p class="body-sm font-bold capitalize text-opacity-50"><slot name="dates" /></p>
 	</div>
-	<div class="w-48 md:w-64">
+	<div class="w-48 md:w-72">
 		<slot name="tags" />
 	</div>
 </div>
