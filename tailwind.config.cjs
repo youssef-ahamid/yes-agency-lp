3189e239aa84377b2b82bf222fb10e572806daec
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-02-12 18:54:18 +0200

    add typography plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -33,5 +33,9 @@ module.exports = {
 			}
 		}
 	},
-	plugins: [require('tailwindcss-animation-delay'), require('tailwind-scrollbar-hide')]
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('tailwindcss-animation-delay'),
+		require('tailwind-scrollbar-hide')
+	]
 };
