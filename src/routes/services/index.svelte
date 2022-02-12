bdae119d96cab12d747ebfd83dd2518f2808edd3
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-02-12 18:56:09 +0200

    dummy routes

---
+++ src/routes/services/index.svelte
@@ -0,0 +1,14 @@
+<script context="module">
+	export async function load({ fetch }) {
+		const res = await fetch('/api/sections')
+        const sections = await res.json();
+        return { props: { sections }}
+	}
+</script>
+
+<script>
+	export let sections;
+</script>
+
+<h1>{ sections[0].title }</h1>
+{@html sections[0].content }
