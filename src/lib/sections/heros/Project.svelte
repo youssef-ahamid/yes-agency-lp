2ea70e0a203b1336513ebcf086b3a700b48b9ea2
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-02-13 09:28:20 +0200

    make project hero shorter

--- src/lib/sections/heros/Project.svelte
+++ src/lib/sections/heros/Project.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <section {id}>
-	<Hero long>
+	<Hero>
 		<div class="max-w-3xl p-4">
 			<div class="-ml-6 md:-ml-10">
 				<Button underlined tertiary to="/projects">/projects/</Button>
