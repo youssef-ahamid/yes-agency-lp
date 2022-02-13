6f33eee88132ffa2f874bfbb839a89fa87162354
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-02-13 13:46:26 +0200

    layout fixes

--- src/lib/components/ui/Hero.svelte
+++ src/lib/components/ui/Hero.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-	class="flex min-h-screen w-full items-center justify-center pt-12 md:-mt-28 md:pt-0"
+	class="flex min-h-screen w-full items-center justify-center pt-12 md:-mt-28 md:pt-0 max-w-[90%] mx-auto"
 >
 	<div
 		class="flex w-full max-w-7xl flex-wrap md:flex-nowrap items-center justify-start overflow-scroll px-2 md:justify-between"
