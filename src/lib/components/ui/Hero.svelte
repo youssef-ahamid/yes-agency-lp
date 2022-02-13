380ef390cfcde64352c8050a2513ca34ef69967a
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-02-13 09:36:50 +0200

    remove long + improve md+ layouts

--- src/lib/components/ui/Hero.svelte
+++ src/lib/components/ui/Hero.svelte
@@ -1,20 +1,16 @@
 <script>
 	import ScrollDown from '$lib/components/base/ScrollDown.svelte';
-	export let long;
 </script>
 
 <div
-	class="flex min-h-screen w-full items-center justify-center pt-12 md:-mt-28"
-	class:pt-28={long}
+	class="flex min-h-screen w-full items-center justify-center pt-12 md:-mt-28 md:pt-0"
 >
 	<div
 		class="flex w-full max-w-7xl flex-wrap md:flex-nowrap items-center justify-start overflow-scroll px-2 md:justify-between"
 	>
 		<slot />
-		{#if !long}
-			<div class="absolute right-2 bottom-2">
-				<ScrollDown />
-			</div>
-		{/if}
+		<div class="absolute right-2 bottom-2">
+			<ScrollDown />
+		</div>
 	</div>
 </div>
