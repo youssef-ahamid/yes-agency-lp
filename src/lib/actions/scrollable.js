9a80e486fd67e061e2dd4638f1b85fad0d7d809e
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-02-14 13:17:27 +0200

    disable modal scroll

---
+++ src/lib/actions/scrollable.js
@@ -0,0 +1,20 @@
+const wheel = function(node, options) {
+    let { scrollable } = options;
+    
+    const handler = e => {
+        if (!scrollable) e.preventDefault();
+    };
+    
+    node.addEventListener('wheel', handler, { passive: false });
+    
+    return {
+        update(options) {
+            scrollable = options.scrollable;
+        },
+        destroy() {
+            node.removeEventListener('wheel', handler, { passive: false });
+        }
+    };
+};
+
+export default wheel;
