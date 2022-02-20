f820e8342710e2d969e508d511ea83e15b682383
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-02-20 15:23:57 +0200

    seo component

---
+++ src/lib/components/seo/index.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { siteLanguage, siteTitle } from '$lib/config/website';
+   
+    export let description;
+    export let title;
+   
+    const pageTitle = `${siteTitle} | ${title}`;
+  </script>
+    
+  <svelte:head>
+    <title>{pageTitle}</title>
+    <meta name="description" content={description} />
+    <meta
+      name="robots"
+      content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1"
+    />
+    <html lang={siteLanguage} />
+  </svelte:head>
